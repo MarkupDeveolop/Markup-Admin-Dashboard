a3b3ce3636881df9ee048d658077de981bca2aea
MarkupDeveolop <markupdevelopment@gmail.com>
2025-11-12 20:30:06 +0200

    updadte this concepts

--- src/lib/constains/constains.ts
+++ src/lib/constains/constains.ts
@@ -3,4 +3,5 @@
 export const DOMAIN = `https://markup-admin-dashboard.vercel.app` ;
 
 // export const FRONTEND = `https://mansoursweet.com`;
+// export const FRONTEND = `https://mansoursweet.com`;
 export const FRONTEND = `http://localhost:3000`;
