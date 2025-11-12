cd3169b861dc4ebba2f77dc47ff527f6c9a510a0
MarkupDeveolop <markupdevelopment@gmail.com>
2025-11-12 21:16:09 +0200

    updated this

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,10 +1,10 @@
 import prisma from "@/lib/prismaDB/prismadb";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const body = await request.json();
-  const { name, email, password, phone } = body;
+  const { name, email, password, phone, } = body;
 
 
   const hashedPassword = await bcrypt.hash(password, 10);
